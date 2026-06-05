2f66dfde574ac43417879f5fa7b59acd3c1b7b2a
ANGEL-LIRA-DEV <angel.lira.work@gmail.com>
2026-06-05 13:14:18 -0600

    Delete security directory

--- security/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.angel.security.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) { super(message); }
-
-}
